18fd4d5f9c438b096257b755a86147d94d3cc86f
Vitor <seuemail>
2024-03-07 15:02:15 -0300

    aaaaa

---
+++ teste.py
@@ -0,0 +1,62 @@
+#####################################################
+# Camada Física da Computação
+#Carareto
+#11/08/2022
+#Aplicação
+###################################################
+
+
+#esta é a camada superior, de aplicação do seu software de comunicação serial UART.
+#para acompanhar a execução e identificar erros, construa prints ao longo do código! 
+
+
+from enlace import *
+import time
+import numpy as np
+import random
+from funcao import *
+
+# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#use uma das 3 opcoes para atribuir à variável a porta usada
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName = "COM4"                  # Windows(variacao de)
+
+
+def main():
+    try:
+        print("Iniciou o main")
+        #declaramos um objeto do tipo enlace com o nome "com". Essa é a camada inferior à aplicação. Observe que um parametro
+        #para declarar esse objeto é o nome da porta.
+        com1 = enlace(serialName)
+        
+    
+        # Ativa comunicacao. Inicia os threads e a comunicação seiral 
+        com1.enable()
+        #Se chegamos até aqui, a comunicação foi aberta com sucesso. Faça um print para informar.
+        print("Abriu a comunicação")
+        imageR = open("image2.png", "rb").read()
+           
+                  
+        #aqui você deverá gerar os dados a serem transmitidos. 
+        #seus dados a serem transmitidos são um array bytes a serem transmitidos. Gere esta lista com o 
+        #nome de txBuffer. Esla sempre irá armazenar os dados a serem enviados.
+        
+        #txBuffer = imagem em bytes!
+        #txBuffer = b'\x12\x13\xAA'  #isso é um array de bytes
+        pacote3 = cria_pacote3(imageR)
+        #envia pacote 3
+        for i in pacote3:
+            com1.sendData(i)
+            print("Enviou pacote 3")
+            time.sleep(0.1)
+    except:
+        print("Erro ao abrir a comunicação")
+
+if __name__ == "__main__":
+    main()
+#         com1.sendData(pacote3)
